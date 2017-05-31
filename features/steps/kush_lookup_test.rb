0e09224d93fdedaddc4147aab5ffc49b9205d726
Gary Peach <gpeach@mdlive.com>
2017-05-31 12:21:56 -0400

    Chromedriver acceptance test working with spinach 3

--- features/steps/kush_lookup_test.rb
+++ features/steps/kush_lookup_test.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 class Spinach::Features::KushLookupTest < Spinach::FeatureSteps
   step 'I have the strain search form' do
-    
     visit 'http://localhost:3000'
     #expect(page).not_to have_selector('body[unresolved]') 
   end
@@ -10,7 +9,6 @@ class Spinach::Features::KushLookupTest < Spinach::FeatureSteps
   step 'I fill in kush' do
     fill_in 'strain', :with => 'kush'
     #find('strain').set('kush')
-   
   end
 
   step 'I submit the form' do
@@ -19,7 +17,6 @@ class Spinach::Features::KushLookupTest < Spinach::FeatureSteps
     #binding.pry
     #screenshot_and_open_image
     loop until page.evaluate_script('jQuery.active').try(:zero?)
-
   end
 
   step 'results should include a paper-card element' do  
